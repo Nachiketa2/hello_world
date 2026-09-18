db1d15eefeb241ca40c91bb52bc90ed15d7c90f1
Nachiketa Gupta <nachiketagupta2@gmail.com>
2026-09-18 14:07:04 +0000

    Add calculator program

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Hello World
+# Calculator
 
-A minimal C++ Hello World program.
+A simple command-line calculator written in C++.
 
 ## Build and run
 
 ```sh
-g++ main.cpp -o hello-world
-./hello-world
+g++ main.cpp -o calculator
+./calculator
 ```
+
+Enter an expression such as `12.5 * 4` when prompted.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!\n";
+    double first{};
+    double second{};
+    char operation{};
+
+    std::cout << "Enter an expression (number operator number): ";
+    if (!(std::cin >> first >> operation >> second)) {
+        std::cerr << "Invalid expression.\n";
+        return 1;
+    }
+
+    switch (operation) {
+    case '+':
+        std::cout << first + second << '\n';
+        break;
+    case '-':
+        std::cout << first - second << '\n';
+        break;
+    case '*':
+        std::cout << first * second << '\n';
+        break;
+    case '/':
+        if (second == 0) {
+            std::cerr << "Cannot divide by zero.\n";
+            return 1;
+        }
+        std::cout << first / second << '\n';
+        break;
+    default:
+        std::cerr << "Unsupported operator. Use +, -, *, or /.\n";
+        return 1;
+    }
+
     return 0;
 }
